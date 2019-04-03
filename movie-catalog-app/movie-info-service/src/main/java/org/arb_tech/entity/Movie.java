1ba95b21a7cd97de21973bae6280a884c3b10090
aniketrb-github <aniketbharsakale@gmail.com>
2019-04-03 20:04:20 +0530

    initial commit for movie-info-service

---
+++ movie-catalog-app/movie-info-service/src/main/java/org/arb_tech/entity/Movie.java
@@ -0,0 +1,28 @@
+package org.arb_tech.entity;
+
+public class Movie {
+	private String id;
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Movie(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+}
