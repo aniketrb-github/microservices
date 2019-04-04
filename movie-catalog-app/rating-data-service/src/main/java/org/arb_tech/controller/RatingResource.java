05561419c595ec33425d465314c4dac964d9e09b
aniketrb-github <aniketbharsakale@gmail.com>
2019-04-04 21:14:26 +0530

    added UserRatingVO & corresponding code changes for the same in controller class

--- movie-catalog-app/rating-data-service/src/main/java/org/arb_tech/controller/RatingResource.java
+++ movie-catalog-app/rating-data-service/src/main/java/org/arb_tech/controller/RatingResource.java
@@ -1,6 +1,11 @@
 package org.arb_tech.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.arb_tech.entity.Rating;
+import org.arb_tech.entity.UserRatingVO;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,4 +19,17 @@ public class RatingResource {
 		return new Rating(movieId, 4);
 	}
 	
+	@GetMapping("/users/{userId}")
+	public UserRatingVO getUserRating (@PathVariable("userId") String userId) {
+		List<Rating> ratingsList = Arrays.asList(
+				new Rating("transformer_2018", 4),
+				new Rating("bumblebee_2019", 5),
+				new Rating("wonder_woman_2019", 2),
+				new Rating("avengers-endgame_2019", 5));
+		
+		UserRatingVO userRatingVO = new UserRatingVO();
+		userRatingVO.setUserRating(ratingsList);
+		return userRatingVO;
+	}
+
 }
