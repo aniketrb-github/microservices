ac0ba2e26fbf3a34ad5f3f9f19655e81ad372f0d
aniketrb-github <aniketbharsakale@gmail.com>
2019-04-04 21:15:33 +0530

    added UserRatingVO & Rating entity as well as the corresponding code changes for the same

--- movie-catalog-app/movie-catalog-service/src/main/java/org/arb_tech/controller/MovieCatalogResource.java
+++ movie-catalog-app/movie-catalog-service/src/main/java/org/arb_tech/controller/MovieCatalogResource.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.arb_tech.entity.CatalogItem;
 import org.arb_tech.entity.Movie;
 import org.arb_tech.entity.Rating;
+import org.arb_tech.entity.UserRatingVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,19 +45,18 @@ public class MovieCatalogResource {
 		// s2: get ratings for each movie by invoking the ratings-data-service
 		// step3: compose s1 & s2 together & return the consolidated data back
 		
-		// this is hardCoded data for time being
-		List<Rating> ratingsList = Arrays.asList(
-				new Rating("transformer_2018", 4),
-				new Rating("bumblebee_2019", 5));
+		UserRatingVO userRatingVO = restTemplate.getForObject("http://localhost:18083/ratingsdata/users/" + userId, UserRatingVO.class);
 
-		// finally send the consolidated data to the UI which contains the CatalogItem as the DTO
-		// this return statement collects the inner returned objects of CatalogItems into a list & sends the List finally
-		return ratingsList.stream().map(rating -> {
-			// Movie movie = restTemplate.getForObject("http://localhost:18082/movies/" + rating.getMovieId(), Movie.class);
-			Movie movie = webClientBuilder.build()
+		return userRatingVO.getUserRating().stream().map(rating -> {
+			Movie movie = restTemplate.getForObject("http://localhost:18082/movies/" + rating.getMovieId(), Movie.class);
+			
+			/* Using WebClient instead of RestTempalte
+			 Movie movie = webClientBuilder.build()
 					.get()
 					.uri("http://localhost:18082/movies/" + rating.getMovieId())
-					.retrieve().bodyToMono(Movie.class).block();
+					.retrieve().bodyToMono(Movie.class)
+					.block();
+			*/
 			
 			return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
 		}).collect(Collectors.toList());
---
+++ movie-catalog-app/movie-catalog-service/src/main/java/org/arb_tech/entity/UserRatingVO.java
@@ -0,0 +1,16 @@
+package org.arb_tech.entity;
+
+import java.util.List;
+
+public class UserRatingVO {
+	private List<Rating> userRating;
+
+	public List<Rating> getUserRating() {
+		return userRating;
+	}
+
+	public void setUserRating(List<Rating> userRating) {
+		this.userRating = userRating;
+	}
+
+}
