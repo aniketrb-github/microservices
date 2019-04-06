f6934b786523a7038d74cf92e35907b30d6f537b
aniketrb-github <aniketbharsakale@gmail.com>
2019-04-06 20:59:27 +0530

    added basic documentation for main app & test class

--- movie-catalog-app/discovery-server/src/main/java/org/arb_tech/DiscoveryServerApplication.java
+++ movie-catalog-app/discovery-server/src/main/java/org/arb_tech/DiscoveryServerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * The Netflix O.S Eureka Server
+ * @author Aniket Bharsakale
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
