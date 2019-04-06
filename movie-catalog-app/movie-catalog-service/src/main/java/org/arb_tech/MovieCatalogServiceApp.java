b0bc01d8a3255f4f2c2cd6e8b2a32d8b41a0ccce
aniketrb-github <aniketbharsakale@gmail.com>
2019-04-07 00:33:37 +0530

    added EurekaClient anotation

--- movie-catalog-app/movie-catalog-service/src/main/java/org/arb_tech/MovieCatalogServiceApp.java
+++ movie-catalog-app/movie-catalog-service/src/main/java/org/arb_tech/MovieCatalogServiceApp.java
@@ -2,6 +2,7 @@ package org.arb_tech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,6 +13,7 @@ import org.springframework.web.reactive.function.client.WebClient;
  * @author Aniket Bharsakale
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApp {
 
 	public static void main(String[] args) {
