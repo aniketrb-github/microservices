ca5e83faef43ec95311467eb1f87f15eb06daf7d
aniketrb-github <aniketbharsakale@gmail.com>
2019-04-11 16:31:11 +0530

    minor code refactoring & code documenting done

--- movie-catalog-app/rating-data-service/src/main/java/org/arb_tech/controller/RatingResource.java
+++ movie-catalog-app/rating-data-service/src/main/java/org/arb_tech/controller/RatingResource.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Entry point of the Ratings-Data-Service which acts as the Rest Controller 
+ * @author Aniket Bharsakale
+ */
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingResource {
@@ -19,16 +23,27 @@ public class RatingResource {
 		return new Rating(movieId, 4);
 	}
 	
+	/**
+	 * fetches ratings list for the given userId
+	 * @param userId
+	 * @return
+	 */
 	@GetMapping("/users/{userId}")
 	public UserRatingVO getUserRating (@PathVariable("userId") String userId) {
+		UserRatingVO userRatingVO = null;
+		
+		// TODO: DB Connection to fetch movie-ratings data
+		// Hard Coded data sent for time-being so as to focus on microservices learning
 		List<Rating> ratingsList = Arrays.asList(
 				new Rating("transformer_2018", 4),
 				new Rating("bumblebee_2019", 5),
 				new Rating("wonder_woman_2019", 2),
 				new Rating("avengers-endgame_2019", 5));
 		
-		UserRatingVO userRatingVO = new UserRatingVO();
-		userRatingVO.setUserRating(ratingsList);
+		if(null != ratingsList && !ratingsList.isEmpty()) {
+			userRatingVO = new UserRatingVO();
+			userRatingVO.setUserRating(ratingsList);
+		}
 		return userRatingVO;
 	}
 
