2b1eda22a33657ca4847f7beac46dc43f543bcaa
aniketrb-github <aniketbharsakale@gmail.com>
2020-02-17 02:18:17 +0530

    enabling config server

--- udemy-microservices/spring-cloud-config-server/src/main/java/com/in28min/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ udemy-microservices/spring-cloud-config-server/src/main/java/com/in28min/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,12 +2,15 @@ package com.in28min.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Entry point for this Spring Cloud Config Server
  *
  * @author Aniket Bharsakale
  */
+
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
