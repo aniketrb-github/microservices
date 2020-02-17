a64db1adcd7f6b907fa64c03ee629b9808bf91ff
aniketrb-github <aniketbharsakale@gmail.com>
2020-02-17 16:53:32 +0530

    documented multiple API's of multiple environments for usage

--- udemy-microservices/spring-cloud-config-server/src/main/java/com/in28min/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ udemy-microservices/spring-cloud-config-server/src/main/java/com/in28min/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -17,5 +17,16 @@ public class SpringCloudConfigServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
+	
+	/**
+	 * API's to retrieve the Configuration data from Git repository for following environments
+	 * ----------------------------------------------------------------------------------------------------------
+	 * ENVIRONMENT	|	SPRING-CONFIG-SERVICE-FILENAME		|	API												|
+	 * ----------------------------------------------------------------------------------------------------------
+	 * default		|	limits-service.properties			|	http://localhost:8888/limits-service/default 	|
+	 * QA			|	limits-service-qa.properties		|	http://localhost:8888/limits-service/qa			|
+	 * DEV			|	limits-service-dev					|	http://localhost:8888/limits-service/dev		|
+	 * ----------------------------------------------------------------------------------------------------------
+	 */
 
 }
