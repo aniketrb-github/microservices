e11119a8bf3d6e693f26f16ad8108cca3b8ce084
aniketrb-github <aniketbharsakale@gmail.com>
2020-02-18 00:50:28 +0530

    reflective code changes after core model class was modified

--- udemy-microservices/currency-conversion-service/src/main/java/com/in28min/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ udemy-microservices/currency-conversion-service/src/main/java/com/in28min/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -3,13 +3,14 @@ package com.in28min.microservices.currencyconversionservice.controller;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.in28min.microservices.currencyconversionservice.bean.ConvertedCurrency;
+import com.in28min.microservices.currencyconversionservice.bean.CurrencyConversionBean;
 import com.in28min.microservices.currencyconversionservice.bean.ExchangeValue;
 
 /**
@@ -23,26 +24,40 @@ public class CurrencyConversionController {
 
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private Environment environment;
 
+	/**
+	 * API: http://localhost:8100/currency-converter/from/AUD/to/INR/amount/1000
+	 * 
+	 * @param from is the source currency from which currency will be converted 
+	 * @param to is the target currency into which currency will be converted
+	 * @param amount to be calculated for 
+	 * @return the total currency amount converted after using the currency exchange values
+	 */
 	@GetMapping(value = "/from/{from}/to/{to}/amount/{amount}")
-	public ConvertedCurrency calculateGrossAmount(@PathVariable String from, @PathVariable String to,
-			@PathVariable Double amount) {
+	public CurrencyConversionBean calculateGrossAmount(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal amount) {
 
-		// http://localhost:8000/currency-exchange/from/USD/to/INR
-		ConvertedCurrency convertedCurrency = null;
+		CurrencyConversionBean currencyBean = null;
 		
 		ExchangeValue exchangeValue = restTemplate.getForObject(
 				"http://localhost:8000/currency-exchange/from/" + from + "/to/" + to, ExchangeValue.class);
 
 		if (null != exchangeValue) {
-			convertedCurrency = new ConvertedCurrency();
-			convertedCurrency.setTo(exchangeValue.getTo());
-			convertedCurrency.setFrom(exchangeValue.getFrom());
-			convertedCurrency.setId(exchangeValue.getId());
-			Double convertedResult = exchangeValue.getConversionMultiple().doubleValue() * amount;
-			convertedCurrency.setConvertedCurrency(BigDecimal.valueOf(convertedResult));
+			currencyBean = new CurrencyConversionBean();
+			currencyBean.setTo(exchangeValue.getTo());
+			currencyBean.setFrom(exchangeValue.getFrom());
+			currencyBean.setId(exchangeValue.getId());
+			currencyBean.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+			currencyBean.setConversionMultiple(exchangeValue.getConversionMultiple());
+			currencyBean.setQuantity(amount);
+			
+			BigDecimal convertedResult = exchangeValue.getConversionMultiple().multiply(amount);
+			currencyBean.setTotalCalculatedAmount(convertedResult);
 		}
 
-		return convertedCurrency;
+		return currencyBean;
 	}
 }
