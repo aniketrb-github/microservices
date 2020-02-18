7d165adf05f8d9688966e67835b171754bc18a59
aniketrb-github <aniketbharsakale@gmail.com>
2020-02-19 00:30:03 +0530

    enabled feign client in this service.

--- udemy-microservices/currency-conversion-service/src/main/java/com/in28min/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ udemy-microservices/currency-conversion-service/src/main/java/com/in28min/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.in28min.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients("com.in28min.microservices.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
