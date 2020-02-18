def98088de5fce5c48602ec552e16f2d4bce1a5d
aniketrb-github <aniketbharsakale@gmail.com>
2020-02-19 00:28:34 +0530

    added feign proxy to communicate wid external services

---
+++ udemy-microservices/currency-conversion-service/src/main/java/com/in28min/microservices/currencyconversionservice/feign/CurrencyExchangeProxy.java
@@ -0,0 +1,31 @@
+package com.in28min.microservices.currencyconversionservice.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.in28min.microservices.currencyconversionservice.bean.ExchangeValue;
+
+/**
+ * Feign Proxy is used to communicate with other micro-services.
+ * Its just like the Rest-Template, but reduces a lot of boiler plate code.
+ * Conceptually its also similar to the JPARepository interface which allows us to transact with a database 
+ *
+ * @author Aniket Bharsakale
+ */
+
+@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+	/**
+	 * name: is the name of the target micro-service with which communication is to be done.
+	 * url : is the URL on which the target micro-service is hosted upon 
+	 */
+	
+	
+	/**
+	 * Below is the copied-pasted method signature from the 'currency-exchange-service' - rest end-point with which we will be communicating
+	 */
+	@GetMapping(value = "/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+}
