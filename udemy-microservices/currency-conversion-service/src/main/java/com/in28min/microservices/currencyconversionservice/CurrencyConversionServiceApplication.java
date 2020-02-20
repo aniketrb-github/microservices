2eac108842e4d692f9121c7d8e07538d3396f40c
aniketrb-github <aniketbharsakale@gmail.com>
2020-02-21 00:58:49 +0530

    making this module as a eureka client

--- udemy-microservices/currency-conversion-service/src/main/java/com/in28min/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ udemy-microservices/currency-conversion-service/src/main/java/com/in28min/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,12 +2,14 @@ package com.in28min.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients("com.in28min.microservices.currencyconversionservice")
+@EnableDiscoveryClient // making this application as an client for eureka server
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
