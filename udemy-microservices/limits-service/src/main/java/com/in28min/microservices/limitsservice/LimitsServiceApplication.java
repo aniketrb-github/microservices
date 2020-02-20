b41cd802116f58a9ae7b17acd8482044b0997379
aniketrb-github <aniketbharsakale@gmail.com>
2020-02-21 01:01:01 +0530

    making this module as a eureka client

--- udemy-microservices/limits-service/src/main/java/com/in28min/microservices/limitsservice/LimitsServiceApplication.java
+++ udemy-microservices/limits-service/src/main/java/com/in28min/microservices/limitsservice/LimitsServiceApplication.java
@@ -2,12 +2,14 @@ package com.in28min.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 /**
  * Entry point of the limits-service application
  *
  * @author Aniket Bharsakale
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
