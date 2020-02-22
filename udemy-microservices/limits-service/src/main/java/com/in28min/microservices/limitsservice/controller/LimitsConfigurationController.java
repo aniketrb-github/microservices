f5134b105c9dfd116461c2913edbf44aa5c9c6b1
aniketrb-github <aniketbharsakale@gmail.com>
2020-02-23 04:13:21 +0530

    added Logger

--- udemy-microservices/limits-service/src/main/java/com/in28min/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ udemy-microservices/limits-service/src/main/java/com/in28min/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,5 +1,7 @@
 package com.in28min.microservices.limitsservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +19,16 @@ import com.in28min.microservices.limitsservice.config.Configuration;
 @RequestMapping(value = "/limits")
 public class LimitsConfigurationController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private Configuration configuration;
 	
 	@GetMapping
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
-		return new LimitConfiguration(configuration.getMinimum(),  configuration.getMaximum());
+		LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMinimum(),
+				configuration.getMaximum());
+		logger.info("{}" + configuration);
+		return limitConfiguration;
 	}
 }
