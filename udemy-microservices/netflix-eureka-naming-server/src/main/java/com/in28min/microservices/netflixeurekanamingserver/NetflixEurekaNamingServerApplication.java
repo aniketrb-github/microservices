40c32798c9e0bd94a38f16a99f25ceae84bb9327
aniketrb-github <aniketbharsakale@gmail.com>
2020-02-22 21:58:16 +0530

    netflix eureka naming server added

---
+++ udemy-microservices/netflix-eureka-naming-server/src/main/java/com/in28min/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -0,0 +1,18 @@
+package com.in28min.microservices.netflixeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ * The Netflix Eureka Naming Server
+ * @author Aniket Bharsakale
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class NetflixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
+	}
+
+}
