e08db41b09acd4ccdcaf5e8a4d632f1312d3856c
aniketrb-github <aniketbharsakale@gmail.com>
2020-02-28 00:50:04 +0530

    impl. Environment object to read application.properties

--- javabrains-microservices/communication-serviceDiscovery-level_1/movie-info-service/src/main/java/org/javabrains/movieinfoservice/controller/MovieController.java
+++ javabrains-microservices/communication-serviceDiscovery-level_1/movie-info-service/src/main/java/org/javabrains/movieinfoservice/controller/MovieController.java
@@ -4,6 +4,7 @@ import org.javabrains.movieinfoservice.model.Movie;
 import org.javabrains.movieinfoservice.model.MovieSummary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,9 @@ public class MovieController {
 
 	@Value("${api.key}")
 	private String API_KEY;
+	
+	@Autowired
+    private Environment environment;
 
 	@Autowired
 	private RestTemplate restTemplate;
@@ -22,7 +26,7 @@ public class MovieController {
 	@RequestMapping("/{movieId}")
 	public Movie getMovieInfo(@PathVariable int movieId) {
 		MovieSummary movieSummary = restTemplate.getForObject(
-				"https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + API_KEY, MovieSummary.class);
+				"https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + environment.getProperty("api.key"), MovieSummary.class);
 		return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
 	}
 }
