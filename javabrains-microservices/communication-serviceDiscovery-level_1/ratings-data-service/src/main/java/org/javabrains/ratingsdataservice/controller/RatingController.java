55f62975da93443fcc4e2a373503a514f3c1b4ac
aniketrb-github <aniketbharsakale@gmail.com>
2020-03-01 03:14:11 +0530

    add java doc.

--- javabrains-microservices/communication-serviceDiscovery-level_1/ratings-data-service/src/main/java/org/javabrains/ratingsdataservice/controller/RatingController.java
+++ javabrains-microservices/communication-serviceDiscovery-level_1/ratings-data-service/src/main/java/org/javabrains/ratingsdataservice/controller/RatingController.java
@@ -4,21 +4,22 @@ import java.util.Arrays;
 
 import org.javabrains.ratingsdataservice.model.Rating;
 import org.javabrains.ratingsdataservice.model.UserRatingsTO;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
+/**
+ * The controller for MovieRatings given by User for his/her watched movies
+ *  
+ * @author Aniket Bharsakale
+ */
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingController {
-	
-	@Autowired
-	private RestTemplate restTemplate;
 
 	// For a user, return all ratings for all his watched movies
-	@RequestMapping("/users/{userId}")
+	@GetMapping("/users/{userId}")
 	public UserRatingsTO getRatingsList(@PathVariable String userId) {
 		UserRatingsTO ratingsTO = new UserRatingsTO();
 		ratingsTO.setRatingsList(Arrays.asList(new Rating(1523, 3.9f), new Rating(550, 4.6f)));
