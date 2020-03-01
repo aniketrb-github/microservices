1e3d1ce8ed9deb49008fc75a9ddbcf17f8e126cd
aniketrb-github <aniketbharsakale@gmail.com>
2020-03-01 16:35:59 +0530

    add Bulkhead pattern behaviour for separate thread pools

--- javabrains-microservices/communication-serviceDiscovery-level_1/movie-catalog-service/src/main/java/org/javabrains/moviecatalogservice/service/MovieInfoService.java
+++ javabrains-microservices/communication-serviceDiscovery-level_1/movie-catalog-service/src/main/java/org/javabrains/moviecatalogservice/service/MovieInfoService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 /**
  * Service class impl. for Movie Info
@@ -28,7 +29,13 @@ public class MovieInfoService {
 	 * @param userRatingTO
 	 * @return
 	 */
-	@HystrixCommand(fallbackMethod = "getMovieInfoFallback")
+	@HystrixCommand(
+			fallbackMethod = "getMovieInfoFallback", 
+			threadPoolKey = "movieInfoPool", 
+			threadPoolProperties = {
+						@HystrixProperty(name = "coreSize", value = "20"), 
+						@HystrixProperty(name = "maxQueueSize", value = "10") 
+									})
 	public List<CatalogItem> getMovieInfo(UserRatingsTO userRatingTO) {
 		List<CatalogItem> catalogItems = userRatingTO.getRatingsList()
 				.stream()
